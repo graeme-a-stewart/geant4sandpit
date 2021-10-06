e6d6e006dcc4dcd87581e52e354fec55bfd0d189
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2021-10-06 18:07:03 +0200

    Add main CMakeLists and clang-format target
    Much easier to keep the C++ clode in good shape with
    a direct clang-format target

---
+++ cmake/clang-format.cmake
@@ -0,0 +1,24 @@
+# Additional target to perform clang-format run
+#
+# Requires clang-format to be available in the
+# environment
+
+# Setup the target version of clang-format we will use
+set(CLANG_FORMAT "clang-format" CACHE STRING "Clang format binary")
+message(STATUS "Setting clang-format test binary to '${CLANG_FORMAT}' (use -DCLANG_FORMAT to change)")
+
+# Get all project files
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *cc *.h *.hh)
+
+# Using --style=Google uses the built in rules of
+# the clang format binary. This is slightly 
+# preferred over an explicit file as new options
+# appear in later versions that cause earlier
+# versions to baulk
+add_custom_target(
+        clang-format
+        COMMAND ${CLANG_FORMAT}
+        --style=Google
+        -i
+        ${ALL_SOURCE_FILES}
+)
